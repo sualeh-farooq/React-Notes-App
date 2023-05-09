990565056f3dc33509c3f592183ce683affddf85
Sualeh Farooq <sualehfarooq65@gmail.com>
2023-05-09 03:35:45 -0700

    initialize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+// import tawkMessengerReactUmd from '@tawk.to/tawk-messenger-react';
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react"
 
 function App() {
   return (
     <div className="App">
+      <TawkMessengerReact propertyId="1h000s9dh" widgetId="1h000s9dh" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
